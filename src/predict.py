1f17a6b2673ca4fb082fc6150ec5b9477954181a
Shringa Bais <21366700+Shringa13@users.noreply.github.com>
2023-05-01 05:25:01 -0700

    Update predict.py

--- src/predict.py
+++ src/predict.py
@@ -2,10 +2,10 @@ import pandas as pd
 from model import read_data,predict_data
 from output_data import output_data_format
 
-def predict(essay_id:str) -> dict:
+def predict(essay_id:str, model) -> dict:
     try:
         processed_output = read_data(essay_id)
-        predicted_data = predict_data(processed_output)
+        predicted_data = predict_data(processed_output, model)
 #         print(predicted_data)
         final_output = output_data_format(predicted_data)
         return final_output
